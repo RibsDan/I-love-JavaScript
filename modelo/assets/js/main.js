2cdd05322c9491469e0ad90520138fd26e55aa7c
Daniel Ribeiro <88308564+RibsDan@users.noreply.github.com>
2021-09-15 09:05:39 -0300

    confirmado

---
+++ modelo/assets/js/main.js
@@ -0,0 +1,25 @@
+const elementos = [
+    {tag:'p', texto:'texto 1'},
+    {tag:'div', texto:'texto 2'},
+    {tag:'section', texto:'texto 3'},
+    {tag:'footer', texto:'texto 4'}
+];
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i]
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+};
+container.appendChild(div);
+
+// for(let i = 0; i < elementos.length; i++) {
+//     let {tag, texto} = elementos[i]
+//     let tagCriada = document.createElement(tag);
+// outraforma =>|    let textoCriado = document.createTextNode(texto); 
+//              |   tagCriada.appendChild(textoCriado);    
+//     div.appendChild(tagCriada);
+// };
+// container.appendChild(div);
