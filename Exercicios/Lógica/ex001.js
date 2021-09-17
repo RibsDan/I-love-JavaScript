fa5ee3d02729a38494dc3c3d0c2a2d67657033f9
Daniel Ribeiro <88308564+RibsDan@users.noreply.github.com>
2021-09-17 08:10:15 -0300

    exercício lógica

---
+++ Exercicios/Lógica/ex001.js
@@ -0,0 +1,16 @@
+const ePaisagem = (l, a) => l > a;
+console.log(ePaisagem(40, 30));
+
+
+
+
+
+// function ePaisagem(l, a){
+//     if(l > a){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// const imagem = ePaisagem(40, 30);
+// console.log(imagem);
